45cca3b13ff0e8041aa7157a4d1295d67b2ba7d2
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-06 18:07:16 +0300

    cpp sdk update

--- cpp_sdk/src/main.cpp
+++ cpp_sdk/src/main.cpp
@@ -1,5 +1,20 @@
 #include "main.h"
 #include <cstdlib>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <cstring>
+
+#define STORAGE_ID1 "/SHM_USER1"
+#define STORAGE_ID2 "/SHM_USER2"
+
+#define STORAGE_SIZE 32
+
+
+// returns file descriptor to the shared memory segment
+extern "C" int open_shared_segment() {
+
+}
 
 int main() {
     return EXIT_SUCCESS;
