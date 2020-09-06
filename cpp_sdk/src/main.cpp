1999b789fed9856a425aadfd77f6ea89b8ac375b
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-06 18:46:00 +0300

    dirty sample shm linux

--- cpp_sdk/src/main.cpp
+++ cpp_sdk/src/main.cpp
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <cstdlib>
-#include <stdio.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <cstring>
+
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <fcntl.h>
 
 #define STORAGE_ID1 "/SHM_USER1"
 #define STORAGE_ID2 "/SHM_USER2"
@@ -12,10 +11,16 @@
 
 
 // returns file descriptor to the shared memory segment
-extern "C" int open_shared_segment() {
-
-}
+//extern "C" int open_shared_segment() {
+//   int fd = shm_open(STORAGE_ID1, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+//}
 
 int main() {
+//    int fd = shm_open(STORAGE_ID1, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    boost::interprocess::shared_memory_object shm_obj
+            (boost::interprocess::create_only                  //only create
+                    ,"shared_memory"              //name
+                    ,boost::interprocess::read_write                   //read-write mode
+            );
     return EXIT_SUCCESS;
 }
--- go_sdk/main.go
+++ go_sdk/main.go
@@ -1,10 +1,22 @@
 package main
-/*
-#cgo LDFLAGS: -L../lib -lhello
-#include "../lib/hello.h"
- */
-import "C"
+
+import (
+	"os"
+	"syscall"
+	"unsafe"
+)
 
 func main() {
-	C.hello(C.CString("GOLANG!!!"))
+	name := "testtt"
+	name2, err := syscall.BytePtrFromString(name)
+	if err != nil {
+		panic(err)
+	}
+	perm := os.O_RDWR|os.O_CREATE|os.O_EXCL
+	fd, _, errno := syscall.Syscall(syscall.SHM, uintptr(unsafe.Pointer(name2)), uintptr(0600), uintptr(perm))
+	if errno != 0 {
+		panic(errno)
+	}
+
+	println(fd)
 }
