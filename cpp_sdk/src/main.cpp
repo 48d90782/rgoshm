df446a8dc3bf5c2eb23cf31247deeacaecb2e304
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-07 16:26:09 +0300

    sample 2

--- cpp_sdk/src/main.cpp
+++ cpp_sdk/src/main.cpp
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <cstdlib>
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/sync/named_mutex.hpp>
 #include <iostream>
 
 void sample1() {
@@ -15,8 +17,18 @@ void sample1() {
         std::cout << size << std::endl;
 }
 
+void sample2() {
+    boost::interprocess::managed_shared_memory managed_shm {boost::interprocess::open_or_create, "shm", 1024};
+    int *i = managed_shm.find_or_construct<int>("Integer")();
+    boost::interprocess::named_mutex named_mtx{boost::interprocess::open_or_create, "mtx"};
+    named_mtx.lock();
+    ++(*i);
+    std::cout << *i << std::endl;
+    named_mtx.unlock();
+}
+
 int main() {
-    sample1();
+    sample2();
 
     return EXIT_SUCCESS;
 }
