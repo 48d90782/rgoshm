80ae95724595e204d30134958c03f563c11a24a9
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-07 11:12:46 +0300

    Sample 1

--- cpp_sdk/src/main.cpp
+++ cpp_sdk/src/main.cpp
@@ -1,28 +1,22 @@
 #include "main.h"
 #include <cstdlib>
-
 #include <boost/interprocess/shared_memory_object.hpp>
-#include <fcntl.h>
+#include <iostream>
 
-#define STORAGE_ID1 "/SHM_USER1"
-#define STORAGE_ID2 "/SHM_USER2"
+void sample1() {
+    boost::interprocess::shared_memory_object shdmem{boost::interprocess::open_or_create, "Boost",
+                                                     boost::interprocess::read_write};
 
-#define STORAGE_SIZE 32
+    shdmem.truncate(1024);
+    std::cout << shdmem.get_name() << std::endl;
+    boost::interprocess::offset_t size;
 
-//https://theboostcpplibraries.com/boost.interprocess-shared-memory
-// returns file descriptor to the shared memory segment
-//extern "C" int open_shared_segment() {
-//   int fd = shm_open(STORAGE_ID1, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-//}
+    if (shdmem.get_size(size))
+        std::cout << size << std::endl;
+}
 
 int main() {
-//    int fd = shm_open(STORAGE_ID1, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    boost::interprocess::shared_memory_object shm_obj
-            (boost::interprocess::create_only                  //only create
-                    ,"shared_memory"              //name
-                    ,boost::interprocess::read_write                   //read-write mode
-            );
-    shm_obj.truncate(100000);
-    
+    sample1();
+
     return EXIT_SUCCESS;
 }
--- go_sdk/main.go
+++ go_sdk/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"os"
-	"syscall"
-	"unsafe"
-)
-
 func main() {
-	name := "testtt"
-	name2, err := syscall.BytePtrFromString(name)
-	if err != nil {
-		panic(err)
-	}
-	perm := os.O_RDWR|os.O_CREATE|os.O_EXCL
-	fd, _, errno := syscall.Syscall(syscall.SHM, uintptr(unsafe.Pointer(name2)), uintptr(0600), uintptr(perm))
-	if errno != 0 {
-		panic(errno)
-	}
 
-	println(fd)
 }
