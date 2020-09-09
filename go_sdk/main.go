8463fa93d20b86d02a44b99579d48154d6bc7c2e
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-09-09 20:35:51 +0300

    remove old code

--- go_sdk/main.go
+++ go_sdk/main.go
@@ -1,16 +1,21 @@
 package main
-/*
-#cgo LDFLAGS: -L../lib -lshm
-#include "../lib/rgoshm.h"
- */
+
 import (
-	"C"
+	"syscall"
+	"unsafe"
 )
 
 func main() {
-	// "data" is the name of the mapped memory region
-	var a *C.char
-	a = C.ReceiveData(C.CString("data"))
-	str := C.GoString(a)
-	println(str)
+	key, err := syscall.BytePtrFromString("1xfffff")
+	if err != nil {
+		panic(err)
+	}
+
+	r1, r2, errno := syscall.RawSyscall(syscall.SYS_SHMGET, uintptr(unsafe.Pointer(key)),uintptr(101010),  uintptr(0666|IPC_CREAT|IPC_EXCL))
+	if errno != 0 {
+		panic(errno)
+	}
+
+	_ = r1
+	_ = r2
 }
